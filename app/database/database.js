e13505e9367cd095e71817d53a114ce2af5bc94e
Jasmin Salvador <sjasmin.salvador@gmail.com>
2023-06-30 11:18:36 -0400

    actualizando

--- app/database/database.js
+++ app/database/database.js
@@ -1,11 +1,34 @@
 import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
+//dotenv.config({path: `.env.${process.env.NODE_ENV}`})
+dotenv.config();
 
-const sequelize = new Sequelize (
-    "db_bootcamp",
-    "postgres",
-    "123456",
+let database, user, password, host;
+
+if (process.env.NODE_ENV == "production") {
+    database = process.env.DB_DATABASE ;
+    user = process.env.DB_USER;
+    password = process.env.DB_PASSWORD;
+    host = process.env.DB_HOST;
+}else {
+    database ="db_bootcamp";
+    user = "postgres";
+    password ="123456";
+    host ="localhost";
+} 
+console.log(host);
+
+/* const database = process.env.DB_DATABASE ;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const host = process.env.DB_HOST; */
+
+const sequelize = new Sequelize(
+    database,
+    user,
+    password,
     {
-        host: "localhost",
+        host: host,
         dialect: "postgres",
     }
 );
